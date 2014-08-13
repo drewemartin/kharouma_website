4f8751a40d2b35060bfc6fc584dfcfc5d7a4e12e
Drew Martin <drew.e.martin@gmail.com>
2014-08-13 12:42:06 -0400

    added PasswordResets controller

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -4,7 +4,8 @@ Rails.application.config.sorcery.submodules = [:reset_password]
 Rails.application.config.sorcery.configure do |config|
   
   config.user_config do |user|
-    user.username_attribute_names = [:email]  
+    user.username_attribute_names = [:email]
+    user.reset_password_mailer = UserMailer  
   end
 
     config.user_class = "User"
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/create'
+
+  get 'password_resets/edit'
+
+  get 'password_resets/update'
+
   resources :users
 
   root :to => 'users#index'
