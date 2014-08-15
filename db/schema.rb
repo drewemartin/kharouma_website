085b68bea64ca5d3b69ec1e09f9b126feb1f646d
Drew Martin <drew.e.martin@gmail.com>
2014-08-15 18:59:16 -0400

    created user article relations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140814183542) do
+ActiveRecord::Schema.define(version: 20140815214625) do
+
+  create_table "articles", force: true do |t|
+    t.text     "body"
+    t.string   "title"
+    t.string   "last_editor"
+    t.string   "publish_location"
+    t.boolean  "publish_now"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                           null: false
